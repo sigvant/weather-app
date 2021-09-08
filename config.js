9e40e9f665f2518639ad160421b9d5b42422953d
sigvant <bnsviv@gmail.com>
2021-09-08 18:09:58 -0300

    added api key

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {
+    MY_API_TOKEN: '86b625b781f7ebc6b0a33dd8d7c37d66',
+    unsplash_key: '4WbcaBMPhZYKY-B9urmFkmBso7aLdO1hlpva670zSe0',
+    unsplash_secret_key: 'W0S4YZTaIY3CuZ5dXrOK19J88czKDJDQ5UrRhYICz6g'
+}
